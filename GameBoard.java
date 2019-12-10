f09ab82cfe78b02ae25caf93f19b74b62aa51bc9
Colin Mills <cgm71@georgetown.edu>
2019-12-10 14:25:39 -0500

    new GameBoard class

---
+++ GameBoard.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+
+public class GameBoard extends JPanel {
+
+    public static class GamePlayRunnable implements Runnable {
+
+
+        /***/
+        @Override
+        public void run() {
+
+        }//END run()
+
+    }//END GamePlayRunnable
+}//END JPanel
